e3af049c53d3aa865c1f6db121c5c106bb79b8f1
Parth Patel <parth2002715@gmail.com>
2021-10-28 11:02:35 -0400

    Finished java backend capstone

--- src/main/java/com/acme/PRSCapstone/User/UserRepository.java
+++ src/main/java/com/acme/PRSCapstone/User/UserRepository.java
@@ -1,7 +1,16 @@
 package com.acme.PRSCapstone.User;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 	public User findUserByUsernameAndPassword(String username, String password);
+	
+	@Query("select u from User u where u.username = ?1 and u.password = ?2")
+	User findByLogin(String username, String password);
+	
+	@Query("select u from User u where u.lastname = ?1")
+	List<User> findByLastname(String lastname);
 }
